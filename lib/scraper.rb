88485da42ae23e696d129ce13a6be6fece2be82e
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-01-11 19:21:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,8 +17,47 @@ class Scraper
     
 
   def self.scrape_profile_page(profile_url)
-    
-  end
+
+     # make document
+    profile_saw = Nokogiri::HTML(open(profile_url))
+    # keywords to parse urls
+    site_array = ["twitter", "linkedin", "github", "rss"]
+    # empty placeholder hash
+    student_hash = {}
+
+     site_array.each do |site|
+      # check for social links' presence
+      if get_url_by_icon("#{site}", profile_saw).length > 0
+      # update student_hash with found links
+        student_hash.merge!({site.to_sym => get_url_by_icon("#{site}", profile_saw) })
+      end
+    end
+
+     # the word blog isn't in the profile page. rename it from search term rss
+    student_hash[:blog] = student_hash.delete(:rss) if student_hash.has_key?(:rss)
+
+     # easily scrape quote and bio
+    student_hash.merge!({:profile_quote => profile_saw.css(".profile-quote").text })
+    student_hash.merge!({:bio => profile_saw.css(".bio-content p").text })
+
+     #return filled-out hash
+    student_hash
+
+
+
+   end	  end
+
+
+ end	end
+
+     
+23  lib/student.rb
+@@ -1,23 +1,34 @@
+class Student	class Student
+
+
+   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 	  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
+
 
 end
 
