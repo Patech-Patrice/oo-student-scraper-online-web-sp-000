5b79635d61f003a942ef6028638e4eb2ce52646f
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-01-11 19:09:13 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,20 @@
 require 'open-uri'
 require 'pry'
+require "nokogiri"
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    html = open ('./fixtures/student-site/index.html')
+    list = Nokogiri::HTML(html)
+   # returns a list of student names
+    names = list.css(".student-name")
+    names_array = []
+    names.each do |item|
+      names_array << item.text
+    end
+  end  
     
-  end
 
   def self.scrape_profile_page(profile_url)
     
